8fa1065517daf0a92832690f01cdd9fb6f2e241e
Viktoryia Kiyanka <mikisum@mail.ru>
2020-03-02 17:08:30 +0300

    feat: implement functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array) || !array.length) return 0;
+    let minimum = array[0];
+    for (let i = 0; i < array.length; i++){
+      minimum = Math.min(minimum, array[i]);
+  } 
+  return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array) || !array.length) return 0;
+    let maximum = array[0];
+    for (let i = 0; i < array.length; i++){
+      maximum = Math.max(maximum, array[i]);
+  }
+  return maximum;
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || !array.length) return 0; 
+  let sum = array.reduce((sum, current) => sum + current, 0);
+  return sum / array.length;
 }
+
